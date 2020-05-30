0153a4cce9d742b0ed1ab504edb5eb23e0b2da73
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-05-30 16:56:30 +0300

    Update tests.js

--- main/tests.js
+++ main/tests.js
@@ -51,15 +51,25 @@ const assert = require('assert').strict;
 
   console.log('------- SI System -------');
 
+  const obj = {
+    p: 0,
+    f: 0,
+    t: 0,
+  };
+
   for (const test of tests) {
     const [par1, par2, expected, name] = test;
     const result = fn(par1, par2);
+    obj.t++;
     try {
       assert.strictEqual(result, expected, `Error in test "${name}"`);
+      obj.p++;
     } catch (err) {
       console.log(err);
+      obj.f++;
     }
   }
+  console.log('Result:', obj.t + ' tests', obj.p + ' passed', obj.f + ' failed');
 }
 
 // Constants:
@@ -97,13 +107,23 @@ const assert = require('assert').strict;
 
   console.log('------- Constants -------');
 
+  const obj = {
+    p: 0,
+    f: 0,
+    t: 0,
+  };
+
   for (const test of tests) {
     const [par, expected, name] = test;
     const result = fn(par);
+    obj.t++;
     try {
       assert.strictEqual(result, expected, `Error in test "${name}"`);
+      obj.p++;
     } catch (err) {
       console.log(err);
+      obj.f++;
     }
   }
+  console.log('Result:', obj.t + ' tests', obj.p + ' passed', obj.f + ' failed');
 }
